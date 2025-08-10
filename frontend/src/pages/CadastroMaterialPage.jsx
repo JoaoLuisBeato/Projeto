4b4976de4f08fdade2f3f15bca78993238ff7de8
Beato <joaoluisbeato@outlook.com>
2025-08-10 15:47:59 -0300

    Arquivo do banco de dados

--- frontend/src/pages/CadastroMaterialPage.jsx
+++ frontend/src/pages/CadastroMaterialPage.jsx
@@ -324,12 +324,6 @@ function CadastroMaterialPage() {
 
               {/* Botões de Ação */}
               <div className="form-actions">
-                <button type="button" className="btn-secondary">
-                  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M19 12H5M12 19l-7-7 7-7"/>
-                  </svg>
-                  Cancelar
-                </button>
                 
                 <button 
                   type="submit" 
